1cc98635ea19bba0cc3e61640607bd79c264f3b0
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-10-14 15:15:59 +0100

    Implemented images upload

--- backend/src/controllers/OrfanagesController.ts
+++ backend/src/controllers/OrfanagesController.ts
@@ -6,7 +6,16 @@ export default {
     async index(req: Request, res: Response) {
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanage = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find();
+
+        return res.json(orphanages);
+    },
+    async show(req: Request, res: Response) {
+        const { id } = req.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id);
 
         return res.json(orphanage);
     },
@@ -24,6 +33,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage);
 
+        const requestImages = req.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -31,7 +45,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         });
 
         await orphanagesRepository.save(orphanage);
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from './config/upload';
 import OrphanageControllers from './controllers/OrfanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanageControllers.index);
-routes.post('/orphanages', OrphanageControllers.create);
+routes.get('/orphanages/:id', OrphanageControllers.show);
+routes.post('/orphanages', upload.array('images'), OrphanageControllers.create);
 
 export default routes;
